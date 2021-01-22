239f6983dbce67e0c73bb3c76ecf6d40d845f7b5
SophiaMVaughn <cq7614@wayne.edu>
2021-01-22 09:46:14 -0500

    alphabetize and comment adminDB file. alphabetize and comment apptDB file

--- mySQL_adminDB.py
+++ mySQL_adminDB.py
@@ -21,14 +21,28 @@ pwd_context = CryptContext(
         pbkdf2_sha256__default_rounds=30000
     )
 
+#Purpose: returns an array of the search dropdown items (username + first name + last name)
+#   for admin, doctor and patient users 
+def adminAllSearchUsers(cursor, cnx): 
+    query=("SELECT username, fname, lname FROM patientTable")
+    cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
+    nameArr = []
+    for un,fn,ln in cursor:
+        tmp = str(un+" - "+ln+", "+fn)
+        nameArr.append(tmp)
+    query=("SELECT username, fname, lname FROM doctorTable")
+    cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
+    for un,fn,ln in cursor:
+        tmp = str(un+" - "+ln+", "+fn)
+        nameArr.append(tmp)
+    query=("SELECT username, fname, lname FROM adminTable")
+    cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
+    for un,fn,ln in cursor:
+        tmp = str(un+" - "+ln+", "+fn)
+        nameArr.append(tmp)
+    return nameArr
 
-#create
-#update
-#delete
-#list/search ALL users (search + see user accts including drs)
-
-#admin - only search/assign care team/create admin/delete acct
-
+#Purpose: check info sent then (if verified) insert into database. Otherwise, return string error msg.
 def createAdminUser(username, pw, fn, ln, cursor, cnx):
     if(mySQL_userDB.isUsernameTaken(username,cursor, cnx)):
         return "taken error"
@@ -59,6 +73,7 @@ def createAdminUser(username, pw, fn, ln, cursor, cnx):
 
     return "success"
     
+#Purpose: return array of all admin usernames
 def getAllAdminUsers(cursor, cnx):
     query = ("SELECT username, password FROM adminTable")
     cursor.execute(query)
@@ -67,6 +82,7 @@ def getAllAdminUsers(cursor, cnx):
         admins.append(un)
     return admins
 
+#Purpose: check username/password for logging into an admin acct
 def verifyAdminLogin(un, pwrd, cursor, cnx):
     pwd_context = CryptContext(
         schemes=["pbkdf2_sha256"],
@@ -83,26 +99,6 @@ def verifyAdminLogin(un, pwrd, cursor, cnx):
             print("WRONG PASSWORD")
             return False
     return False
-
-def adminAllSearchUsers(cursor, cnx): #fix SQL DONE
-    query=("SELECT username, fname, lname FROM patientTable")
-    cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-    nameArr = []
-    for un,fn,ln in cursor:
-        tmp = str(un+" - "+ln+", "+fn)
-        nameArr.append(tmp)
-    query=("SELECT username, fname, lname FROM doctorTable")
-    cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-    for un,fn,ln in cursor:
-        tmp = str(un+" - "+ln+", "+fn)
-        nameArr.append(tmp)
-    query=("SELECT username, fname, lname FROM adminTable")
-    cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-    for un,fn,ln in cursor:
-        tmp = str(un+" - "+ln+", "+fn)
-        nameArr.append(tmp)
-    return nameArr
-
 
 
 cursor.close()
--- mySQL_apptDB.py
+++ mySQL_apptDB.py
@@ -22,73 +22,7 @@ pwd_context = CryptContext(
         pbkdf2_sha256__default_rounds=30000
     )
 
-
-def getAllApptsFromUsername(username, tempcursor, cursor, cnx):
-    query = ("SELECT mtgID, doctor, patient, mtgName, startTime, joinURL FROM apptTable WHERE doctor = %s OR patient = %s")         
-    cursor.execute(query, (username, username)) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-    apptArr = []
-    for mI, d, p, mN, sT, jU in cursor:  
-        
-        startDate= sT.split('T')[0]
-        if(datetime.now().strftime('%Y-%m-%d')>startDate): #if the date of the appt is past todays date FULLY, archive it
-            archiveAppt(mI, tempcursor, cnx)
-        else: #else it's today or later so keep it in the calendar
-            apptArr.append(apptObjectClass (mI, d, p, mN, sT, jU) ) 
-#TODO -- change this    
-    return apptArr
-
-def isMeetingIDValid(mtgid, cursor, cnx):
-    query = ("SELECT mtgID FROM apptTable WHERE mtgID = %s")         
-    cursor.execute(query, (mtgid, )) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-    for item in cursor:
-        return True
-    return False
-
-def getApptFromMtgId(mtgid, cursor, cnx):
-    query = ("SELECT mtgID, doctor, patient, mtgName, startTime, joinURL FROM apptTable WHERE mtgID = %s")         
-    cursor.execute(query, (mtgid,)) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-    patientArr = []
-    for mI, d, p, mN, sT, jU in cursor:  
-        apptClassObj = apptObjectClass (mI, d, p, mN, sT, jU)   
-        return apptClassObj
-    #TODO -- change this    
-        return (mI, d, p, mN, sT, jU)
-    
-def isMtgStartTimePassed(mtgID, cursor, cnx):
-    print("NOW",datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), "APPT TIME:", getApptFromMtgId(mtgID, cursor, cnx).startTime)
-    if(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")>getApptFromMtgId(mtgID, cursor, cnx).startTime):
-        print("NOW is AFTER mtg start time")
-        #if the start time of the mtg is past the current time, this will return true
-        return True
-    print("NOW IS BEFORE mtg start time")
-    return False
-
-def createAppt(mtgName, mtgid, doctor, patient, start_time, joinURL, cursor, cnx):
-    formatInsert = ("INSERT INTO apptTable "
-                   "(mtgID, doctor,patient,mtgName,"
-                    "startTime,joinURL) "
-                   "VALUES (%s, %s,%s, %s,%s, %s)") #NOTE: use %s even with numbers
-    insertContent = (mtgid, doctor, patient, mtgName, start_time, joinURL)
-    cursor.execute(formatInsert, insertContent)
-    cnx.commit()
-    return "success create"
-
-def deleteAppt(mtgid, cursor, cnx):
-    delete_test = (
-        "DELETE FROM apptTable " #table name NOT db name
-        "WHERE mtgID = %s")
-    cursor.execute(delete_test, (mtgid,))
-    cnx.commit()
-    # for item in cursor:
-    #     return "deleted "+mtgid
-    
-def deleteBadMsgs():
-    delete_test = (
-        "DELETE FROM messageDB " #table name NOT db name
-        "WHERE sender = %s OR reciever = %s")
-    cursor.execute(delete_test, ("deactivatedUser","deactivatedUser"))
-    cnx.commit()
-
+#Purpose: add meeting to archive database and delete if from the current meetings database
 def archiveAppt(mtgid, cursor, cnx):
     apptDetails=getApptFromMtgId(mtgid,cursor,cnx)
     formatInsert = ("INSERT INTO archiveApptTable "
@@ -99,7 +33,20 @@ def archiveAppt(mtgid, cursor, cnx):
     cursor.execute(formatInsert, insertContent)
     cnx.commit()
     deleteAppt(mtgid,cursor,cnx)
-    
+
+#Purpose: add meeting to the current meetings database
+def createAppt(mtgName, mtgid, doctor, patient, start_time, joinURL, cursor, cnx):
+    formatInsert = ("INSERT INTO apptTable "
+                   "(mtgID, doctor,patient,mtgName,"
+                    "startTime,joinURL) "
+                   "VALUES (%s, %s,%s, %s,%s, %s)") #NOTE: use %s even with numbers
+    insertContent = (mtgid, doctor, patient, mtgName, start_time, joinURL)
+    cursor.execute(formatInsert, insertContent)
+    cnx.commit()
+    return "success create"
+
+#Purpose: when a patient deletes their acct, update all archive entries they are a part of to
+#   be a [deleted] username (so they don't show up when you query for original username)
 def deactivateAllArchivedAppts(username, cursor, cnx):
     update_test = (
                 "UPDATE archiveApptTable SET patient=%s "
@@ -107,8 +54,56 @@ def deactivateAllArchivedAppts(username, cursor, cnx):
     cursor.execute(update_test, (str(username+" [deleted]"),username))
     cnx.commit()
     return "success update"
-    
 
+#Purpose: delete meeting from the current meetings database
+def deleteAppt(mtgid, cursor, cnx):
+    delete_test = (
+        "DELETE FROM apptTable " #table name NOT db name
+        "WHERE mtgID = %s")
+    cursor.execute(delete_test, (mtgid,))
+    cnx.commit()
+    # for item in cursor:
+    #     return "deleted "+mtgid
+ 
+#Purpose: return an array of appointment class objects where each obj holds an appt
+#   that the username is involved in (patient or dr)
+def getAllApptsFromUsername(username, tempcursor, cursor, cnx):
+    query = ("SELECT mtgID, doctor, patient, mtgName, startTime, joinURL FROM apptTable WHERE doctor = %s OR patient = %s")         
+    cursor.execute(query, (username, username)) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
+    apptArr = []
+    for mI, d, p, mN, sT, jU in cursor:  
+        
+        startDate= sT.split('T')[0]
+        if(datetime.now().strftime('%Y-%m-%d')>startDate): #if the date of the appt is past todays date FULLY, archive it
+            archiveAppt(mI, tempcursor, cnx)
+        else: #else it's today or later so keep it in the calendar
+            apptArr.append(apptObjectClass (mI, d, p, mN, sT, jU) )  
+    return apptArr
+
+#Purpose: return an appointment class object that holds all details of queried mtg
+def getApptFromMtgId(mtgid, cursor, cnx):
+    query = ("SELECT mtgID, doctor, patient, mtgName, startTime, joinURL FROM apptTable WHERE mtgID = %s")         
+    cursor.execute(query, (mtgid,)) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
+    patientArr = []
+    for mI, d, p, mN, sT, jU in cursor:  
+        apptClassObj = apptObjectClass (mI, d, p, mN, sT, jU)   
+        return apptClassObj
+ 
+#Purpose: return true if appt is in the table, false if it is not
+def isMeetingIDValid(mtgid, cursor, cnx):
+    query = ("SELECT mtgID FROM apptTable WHERE mtgID = %s")         
+    cursor.execute(query, (mtgid, )) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
+    for item in cursor:
+        return True
+    return False
+
+#Purpose: if the start time of the mtg is past the current time, this will return true (otherwise, false)
+def isMtgStartTimePassed(mtgID, cursor, cnx):
+    if(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")>getApptFromMtgId(mtgID, cursor, cnx).startTime):
+        return True
+    return Falses   
+
+#Purpose: update the meeting details of the meeting in the table
 def updateAppt(mtgName, mtgid,start_time, cursor, cnx): 
     update_test = (
                 "UPDATE apptTable SET mtgName=%s, startTime=%s "
@@ -116,11 +111,6 @@ def updateAppt(mtgName, mtgid,start_time, cursor, cnx):
     cursor.execute(update_test, (mtgName,start_time, mtgid))
     cnx.commit()
     return "success update"
-
-# query = ("SELECT * FROM apptTable ")         
-# cursor.execute(query) #NOTE: even if there is only 1 condition, you have to make the item passed to the query into a TUPLE
-# for item in cursor:
-#    print(item) #each item = a row = a tuple
 
 cursor.close()
 cnx.close()
